954220e0d27bc913d123b9311308adae3a41dc4c
Esteban Nahuel Carrizo <estebancarrizo619@gmail.com>
2022-02-07 21:43:45 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 function CambiarColor(color){
-		if(color=='1'){
-			document.body.style.background='#f00';
-		}
-		if(color=='2'){
-			document.body.style.background='#fff';
-		}
-		if(color=='3'){
-			document.body.style.background='#000';
-		}
-		if(color=='4'){
-			document.body.style.background='#2BD56E';
-		}
-	}
+	if(color=='1'){
+	document.body.style.background='#f00';
+}
+	if(color=='2'){
+	document.body.style.background='#fff';
+}
+	if(color=='3'){
+	document.body.style.background='#000';
+}
+	if(color=='4'){
+	document.body.style.background='#2BD56E';
+}
+}
